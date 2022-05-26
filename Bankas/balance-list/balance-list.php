c69e73fae3bfe778bf9e1097bad82e44d549d3f4
TomasUri <urinovic.tomas@gmail.com>
2022-05-26 12:39:52 +0300

    Create php & css files

---
+++ Bankas/balance-list/balance-list.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Balance list</title>
+    <link rel="stylesheet" href="../css/body.css">
+    <link rel="stylesheet" href="../css/styleForBL.css">
+    
+</head>
+<body>
+    <a href="http://localhost/php-bankas/bankas/add-funds/add-funds.php#">Add funds</a>
+    <a href="http://localhost/php-bankas/bankas/deduct-funds/deduct-funds.php#">Deduct funds</a>
+</body>
+</html>
